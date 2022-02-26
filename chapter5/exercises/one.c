e51cb059b25a7c58e852692061de2fd32a3bf175
Alec74 <alecmcglone@gmail.com>
2022-02-26 21:41:13 +0000

    starting chapter 5 exercises

---
+++ chapter5/exercises/one.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main(void)
+{
+	int a;
+	a = 3 % 4;
+	
+	printf("a = : %d\n", a);
+
+	return 0;
+}
---
+++ chapter5/exercises/two.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main(void)
+{
+	int i, j, k;
+	i = 10;
+	j = 5;
+	
+	printf("Evaluates to: %d\n", !i < j);
+
+	return 0;
+}

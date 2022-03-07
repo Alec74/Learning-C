ad1f9141a7a7f640618ffcc4723955d3fbf2ec97
Alec74 <alecmcglone@gmail.com>
2022-03-07 08:47:54 -0500

    project 8.17

--- chapter8/projects/seventeen.c
+++ chapter8/projects/seventeen.c
@@ -1,34 +1,58 @@
 #include <stdio.h>
-#include <ctype.h>
 
-int main(void)
-{
-    char a;
-    int check = 0, i, j, word[26] = {0};
+int main(void) {
 
-    printf("Enter a word: ");
-    while((a = getchar()) != '\n'){
-        j = toupper(a) - 'A';
-        word[j] += 1;
-    }
+    int count, n, x, y, x_last, y_last;
+    
 
-    printf("Enter another word: ");
-    while((a = getchar()) != '\n'){
-        j = toupper(a) - 'A';
-        word[j] -= 1;
-    }
+    printf("This program creates a magic square of a specified size.\n");
+    printf("The size must be an odd number between 1 and 99.\n");
+    printf("Enter size of magic square: ");
+    scanf("%d", &n);
 
-    for(j=0; j<=25; j++){
-        if (word[j] == 0){
-            check++;
+    int square[n][n];
+    
+    for (x = 0; x < n; x++) {
+        for (y = 0; y < n; y++) {
+            square[x][y] = 0;
         }
     }
 
-    if(check == 26){
-        printf("The words are anagrams.\n");
-    }else{
-        printf("The words are not anagrams.\n");
+    x = n / 2; 
+    y = 0;
+    square[x][y] = 1;
+
+    count = 2;
+    while (count <= n * n) {
+        
+        x_last = x;
+        y_last = y;
+
+        if (x_last + 1 >= n)
+            x = 0;
+        else 
+            x = x_last + 1;
+        if (y_last - 1 < 0)
+            y = n - 1;
+        else 
+            y = y_last - 1;
+        
+        if (square[x][y] == 0) {
+            square[x][y] = count++;
+        } else {
+            if (y_last + 1 >= n)
+                square[x = x_last][y = 0] = count++;
+            else
+                square[x = x_last][y = y_last + 1] = count++;
+        }
     }
 
+    for (y = 0; y < n; y++) {
+        for (x = 0; x < n; x++) {
+            printf("%4d", square[x][y]);
+        }
+        printf("\n");
+    }
+    
     return 0;
 }
